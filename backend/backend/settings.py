26f07b751929a5e8ac47cbfe4beb7678045a0233
전민규 <mgjeon@containus.kr>
2023-07-30 18:31:24 +0900

    시크릿키 분리

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from screat.secreat import SECRET_KEY as SK
+from screat.screat import SCREAT_KEY as SK
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
